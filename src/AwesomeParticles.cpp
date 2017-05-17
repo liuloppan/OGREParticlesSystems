bf473375d4e102d7f74c0353cee668c47e60572d
Rosdyana Kusuma <rosdyana.kusuma@gmail.com>
2017-05-18 00:57:39 +0800

    - added shadow to main character - fix y axis of the floor to fix flying character bug

--- src/AwesomeParticles.cpp
+++ src/AwesomeParticles.cpp
@@ -144,7 +144,7 @@ void AwesomeParticles::createScene()
     mChara = new SinbadCharacterController(mCamera);
     // create a floor mesh resource
     MeshManager::getSingleton().createPlane("floor", ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
-                                            Plane(Vector3::UNIT_Y, -30), 10000, 10000, 10, 10, true, 1, 128, 128, Vector3::UNIT_Z);
+                                            Plane(Vector3::UNIT_Y, -18), 10000, 10000, 20, 20, true, 1, 128, 128, Vector3::UNIT_Z);
 
 
     // create a floor entity, give it a material, and place it at the origin
